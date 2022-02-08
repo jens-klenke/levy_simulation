f18c2af15a784593d45f3439e11fc9a2acffb434
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2022-02-08 15:45:28 +0100

    set-up

---
+++ 00_packages/packages.R
@@ -0,0 +1,10 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               ggplot2,
+               here,
+               purrr,
+               magick,
+               dplyr,
+               tidyr)
+
+
---
+++ 01_code/code.R
@@ -0,0 +1,21 @@
+## ---- packages ----
+source(here::here('00_packages/packages.R'))
+
+## ---- Gamma Process simulation ----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
