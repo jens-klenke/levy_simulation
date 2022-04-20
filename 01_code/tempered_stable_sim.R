7c741f0cf2c50f339b50566ae730caa4b1f4e9b4
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2022-04-20 18:41:20 +0200

    update

--- 01_code/tempered_stable_sim.R
+++ 01_code/tempered_stable_sim.R
@@ -7,14 +7,20 @@ source(here::here('00_packages/packages.R'))
 
 ## --- Algorithm 0 ----
 
-U <- runif(1)
 
+b <- 0.1
 alpha <- 0.2
 # beta -> 1; Subordinator; absolut skewness; 
-
 # delta -> 0; location
-# Levy matters IV -> page 215
-S <- stabledist::rstable(1, alpha = alpha, gamma = , beta = 1, delta = 0)
+# gamma -> 
 
+
+U <- runif(1)
+
+# Levy matters IV -> page 215
+V <- stabledist::rstable(1, alpha = alpha, beta = 1, gamma = 1, delta = 0)
+
+
+if(U <= exp(-b*V)) Y <- V
 
 
